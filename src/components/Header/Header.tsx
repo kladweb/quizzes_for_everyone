ae3a6ab42a7853858deceaf2f5d855508cfd1236
Pavel Kladkevich <kladpav@gmail.com>
2025-12-19 16:55:47 +0300

    feat: add toggle theme button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { ThemeToggle } from "../ThemeToggle/ThemeToggle";
+import "./header.css"
 
 export const Header: React.FC = () => {
 
   return (
-    <header>
-      <div></div>
+    <header className="header-container">
+      <ThemeToggle/>
     </header>
   );
 };
