a339ab3682aef91ba70ee6f6a3b58fa83797e067
Pavel Kladkevich <kladpav@gmail.com>
2026-01-20 22:24:30 +0300

    refactor: add navigate for way cards

--- src/pages/PageCreateQuiz/PageCreateQuiz.tsx
+++ src/pages/PageCreateQuiz/PageCreateQuiz.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { CreateQuizWay } from "../../components/CreateQuizWay/CreateQuizWay";
-import { type IWayCardsData, wayCardsData } from "../../components/CreateQuizWay/wayCardsData";
+import {useNavigate} from "react-router-dom";
+import {CreateQuizWay} from "../../components/CreateQuizWay/CreateQuizWay";
+import {type IWayCardsData, wayCardsData} from "../../components/CreateQuizWay/wayCardsData";
 import "./pageCreateQuiz.css";
 
 export const PageCreateQuiz = () => {
-
+  const navigate = useNavigate();
   const handlerCreateWay = (e: React.MouseEvent<HTMLElement>) => {
+    // navigate(`/tests/${testId}`);
+    navigate(`/createquiz/${e.currentTarget.id}`);
     console.log(e.currentTarget.id);
   }
 
