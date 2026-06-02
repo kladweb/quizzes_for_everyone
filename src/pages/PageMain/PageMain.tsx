b753e061b7209a1c79c5effca75ca977439a6d39
Pavel Kladkevich <kladpav@gmail.com>
2026-06-02 16:06:09 +0300

    feat: add select action to main page

--- src/pages/PageMain/PageMain.tsx
+++ src/pages/PageMain/PageMain.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
-import { Navigate, NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { loginGoogle, useIsAuthLoading, useUser } from "../../store/useUserStore";
+import { clearCurrentQuiz } from "../../store/useCurrentCreatingQuiz";
 
 export const PageMain = () => {
+  const navigate = useNavigate();
   const user = useUser();
   const isAuthLoading = useIsAuthLoading();
 
@@ -10,26 +12,37 @@ export const PageMain = () => {
     document.title = "ANY QUIZ";
   }, []);
 
-  if (user) {
-    return <Navigate to="/createquiz" replace/>;
+  const createQuiz = () => {
+    clearCurrentQuiz();
+    navigate("/createquiz");
+  }
+
+  const takeQuiz = () => {
+    navigate("/allquizzes");
   }
 
   return (
-    <>
-      <div className='tests-container'>
-        <NavLink to='/allquizzes'>
-          <img className="main-image" src="open.png" alt="any-quiz" title="Перейти к списку всех тестов"/>
-        </NavLink>
-        <div className='noticeBlock'>
-          <p className='noticeText'>Авторизуйтесь, чтобы начать создавать свои тесты...</p>
-        </div>
-        <button className='btn button-login ' onClick={loginGoogle} disabled={isAuthLoading}>
-          {isAuthLoading ? "GOOGLE IN..." : "GOOGLE LOGIN"}
-        </button>
-        <NavLink className='link-nav link-all-quizzes' to={'/allquizzes'}>
-          <span>Перейти к списку всех тестов</span>
-        </NavLink>
-      </div>
-    </>
+    <div className='tests-container'>
+      <NavLink to='/allquizzes'>
+        <img className="main-image" src="open.png" alt="any-quiz" title="Перейти к списку всех тестов"/>
+      </NavLink>
+      {
+        user ?
+          <div className='noticeBlock'>
+            <p className='noticeText'>Что Вы хотите сделать?</p>
+            <button className='btn button-main' onClick={createQuiz}>Создать новый тест</button>
+            <button className='btn button-main' onClick={takeQuiz}>Пройти тест</button>
+          </div> :
+          <div className='noticeBlock'>
+            <p className='noticeText'>Авторизуйтесь, чтобы начать создавать свои тесты...</p>
+            <button className='btn button-login ' onClick={loginGoogle} disabled={isAuthLoading}>
+              {isAuthLoading ? "GOOGLE IN..." : "GOOGLE LOGIN"}
+            </button>
+          </div>
+      }
+      <NavLink className='link-nav link-all-quizzes' to={'/allquizzes'}>
+        <span>Перейти к списку всех тестов</span>
+      </NavLink>
+    </div>
   );
 }
